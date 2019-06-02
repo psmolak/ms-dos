931bd14e8a2ae74fd7bb1186fd3e4425f463a8cf
Paweł Smolak <smolak.paul@gmail.com>
2019-06-02 22:22:48 +0200

    Dodano wypisywanie komunikatu powitalnego

--- main.asm
+++ main.asm
@@ -1,12 +1,39 @@
-org 100h
+org 0x100
+
+; Program wczytuje plik o nazwie wczytanej z stdin.
+;
+; Jest to prosty automat przetwarzający plik znak
+; po znaku, odpowiednio reagując na napotkanie nowej linii.
+;
+; Warto zadbać o obsługę błędów.
+; Można dodać jakieś kolorki jak starczy czasu.
+
+section .data
+
+	; konfiguracja
+	filename_maxlength     dw 0x100
+
+	; messages
+	fatalmsg      db 'Wystapil nieoczekiwany blad$'
+	intromsg      db 'Podaj nazwe pliku$'
+	invalidmsg    db 'Podany o podanej nazwie nie istnieje$'
+
+
+section .bss
+
+	filename      resb 0x100
+	handler       resw 0x1
+
 
 section .text
 
 start:
-	; put your code here
+	; Wyświetl komunikat powitalny
+	mov ah, 9h
+	mov dx, intromsg
+	int 21h
 
-section .data
-	; put data items here
+exit:
+	mov ah, 4Ch
+	int 21h
 
-section .bss
-	; put uninitialized data here
